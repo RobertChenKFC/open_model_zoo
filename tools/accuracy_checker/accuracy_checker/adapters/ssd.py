5e4b9a823792721e5c83d213655b2ce0bc50ddc3
RobertChenKFC <robert.chen.kfc@gmail.com>
2022-03-22 19:24:22 +0800

    modified WIDER dataset to fit our needs and added face detection adapter

--- tools/accuracy_checker/accuracy_checker/adapters/ssd.py
+++ tools/accuracy_checker/accuracy_checker/adapters/ssd.py
@@ -17,8 +17,10 @@ limitations under the License.
 import itertools
 import math
 import re
+from typing import Tuple
 
 import numpy as np
+import numba as nb
 
 from .adapter import Adapter
 from ..config import ConfigValidator, StringField, NumberField, ListField, BoolField
@@ -372,3 +374,285 @@ class SSDONNXAdapter(Adapter):
         self.bboxes_out = find_layer(bboxes_regex, 'bboxes', raw_outputs)
 
         self.outputs_verified = True
+
+
+class SSDFaceDetectionAdapter(Adapter):
+    __provider__ = "ssd_face_detection"
+
+    @classmethod
+    def parameters(cls):
+        parameters = super().parameters()
+        parameters.update({
+            "box_offsets_out": StringField(
+                description="name (or regex for it) of output layer with "
+                            "bounding box offsets."
+            ),
+            "class_predictions_out": StringField(
+                description="name (or regex for it) of output layer with "
+                            "class predictions."
+            ),
+            "image_width": NumberField(
+                description="width of the model image input"
+            ),
+            "image_height": NumberField(
+                description="height of the model image input"
+            )
+        })
+        return parameters
+
+    def configure(self):
+        self.box_offsets_out = self.get_value_from_config(
+            "box_offsets_out"
+        )
+        self.class_predictions_out = self.get_value_from_config(
+            "class_predictions_out"
+        )
+        self.img_width = np.int32(self.get_value_from_config("image_width"))
+        self.img_height = np.int32(self.get_value_from_config("image_height"))
+        self.prior_boxes = self.generate_all_prior_boxes()
+
+    @staticmethod
+    @nb.njit
+    def generate_prior_boxes(
+        img_width: int, img_height: int, layer_width: int, layer_height: int,
+        aspect_ratios: np.array, min_size: int, max_size: int, step: int,
+        offset: float
+    ) -> np.array:
+        """
+        Generates a np array of shape (1, num_prior_boxes, 4) where each group
+        of 4 values corresponds to a prior boxes' center_x, center_y, box_width,
+        box_height.
+        """
+        # Add all the reciprocals of the aspect ratios, too
+        num_aspect_ratios = len(aspect_ratios)
+        all_aspect_ratios = np.zeros((2 * num_aspect_ratios,), dtype=np.float32)
+        for i in range(num_aspect_ratios):
+            all_aspect_ratios[i * 2] = aspect_ratios[i]
+            all_aspect_ratios[i * 2 + 1] = 1. / aspect_ratios[i]
+
+        num_aspect_ratios = len(all_aspect_ratios)
+        num_prior_boxes = layer_width * layer_height * (num_aspect_ratios + 2)
+        prior_boxes = np.zeros((1, num_prior_boxes, 4), dtype=np.float32)
+
+        # Boxes:
+        # 1.     width:  min_size
+        #        height: min_size
+        # 2.     width:  sqrt(min_size * max_size)
+        #        height: sqrt(min_size * max_size)
+        # i + 2. width:  min_size / sqrt(aspect_ratios[i]),
+        #        height: max_size * sqrt(aspect_ratios[i])
+        box_widths = np.zeros((2 + num_aspect_ratios,), dtype=np.int32)
+        box_heights = np.zeros((2 + num_aspect_ratios,), dtype=np.int32)
+        box_widths[:2] = box_heights[:2] = (
+            min_size, int(np.sqrt(min_size * max_size))
+        )
+        box_widths[2:] = min_size * np.sqrt(all_aspect_ratios)
+        box_heights[2:] = min_size / np.sqrt(all_aspect_ratios)
+
+        # Fill in box coordinates in the first half of the input
+        idx = 0
+        for i in range(layer_height):
+            for j in range(layer_width):
+                center_x = (j + offset) * step
+                center_y = (i + offset) * step
+
+                for k in range(len(box_widths)):
+                    box_width = box_widths[k]
+                    box_height = box_heights[k]
+                    prior_boxes[0, idx, :] = np.array([
+                        center_x / img_width, center_y / img_height,
+                        box_width / img_width, box_height / img_height
+                    ], dtype=np.float32)
+                    idx += 1
+
+        return prior_boxes
+
+    def generate_all_prior_boxes(self):
+        layer_dimensions = [
+            (38, 38), (19, 19), (10, 10), (5, 5), (5, 5), (5, 5)
+        ]
+        all_aspect_ratios = [
+            [2.], [2., 3.], [2., 3.], [2., 3.], [2.], [2.]
+        ]
+        min_max_sizes = [
+            (30, 60), (60, 111), (111, 162), (162, 213), (213, 264), (264, 315)
+        ]
+        steps = [8, 16, 32, 64, 100, 300]
+        all_prior_boxes = np.concatenate([
+            self.generate_prior_boxes(
+                self.img_width, self.img_height, layer_width, layer_height,
+                np.array(aspect_ratio, dtype=np.float32),
+                min_size, max_size, step, 0.5
+            ) for (
+                (layer_width, layer_height), aspect_ratio, (min_size, max_size),
+                step
+            ) in zip(layer_dimensions, all_aspect_ratios, min_max_sizes, steps)
+        ], axis=1)
+        return all_prior_boxes
+
+    @staticmethod
+    @nb.njit
+    def get_bounding_boxes(
+        num_classes: np.int32, box_offsets: np.array,
+        class_predictions: np.array, prior_boxes: np.array,
+        variances: np.array, confidence_threshold: np.float32
+    ) -> Tuple[np.array, np.array]:
+        """Returns two np arrays: bounding_boxes and classes. Bounding_boxes
+        is of shape (1, num_prior_boxes, 5), and each group of 5 values
+        corresponds to min x, min y, max x, max y, confidence. Classes is of shape
+        (1, num_prior_boxes), where each value corresponds to the class index.
+        """
+        num_prior_boxes = prior_boxes.shape[1]
+        bounding_boxes = np.zeros((1, num_prior_boxes, 5), dtype=np.float32)
+        classes = np.zeros((1, num_prior_boxes), dtype=np.int32)
+
+        (
+            center_x_variance, center_y_variance,
+            box_width_variance, box_height_variance
+        ) = variances
+        idx = 0
+        for i in range(num_prior_boxes):
+            max_prob_class = np.argmax(
+                class_predictions[
+                    0,
+                    i * (num_classes + 1) + 1:
+                    i * (num_classes + 1) + 1 + num_classes
+                ]
+            ) + i * (num_classes + 1) + 1
+            max_prob = class_predictions[0, max_prob_class]
+            if max_prob < confidence_threshold:
+                continue
+            (
+                center_x_offset, center_y_offset,
+                box_width_offset, box_height_offset
+            ) = box_offsets[0, i * 4: (i + 1) * 4]
+
+            center_x, center_y, box_width, box_height = prior_boxes[0, i, :]
+            center_x += center_x_offset * box_width * center_x_variance
+            center_y += center_y_offset * box_height * center_y_variance
+            box_width *= np.exp(box_width_offset * box_width_variance) / 2
+            box_height *= np.exp(box_height_offset * box_height_variance) / 2
+
+            bounding_boxes[0, idx, :] = np.array([
+                # min x
+                center_x - box_width,
+                # min y
+                center_y - box_height,
+                # max x
+                center_x + box_width,
+                # max y
+                center_y + box_height,
+                # confidence
+                max_prob
+            ], dtype=np.float32)
+            classes[0, idx] = max_prob_class
+            idx += 1
+
+        return bounding_boxes[:, :idx, :], classes[:, :idx]
+
+    @staticmethod
+    @nb.njit
+    def nms(
+        bounding_boxes: np.array, classes: np.array, nms_threshold: float
+    ) -> Tuple[np.array, np.array]:
+        """
+        Uses Non-Maximum Suppression to extract the bounding boxes and its
+        corresponding class indices together.
+        """
+        num_boxes = bounding_boxes.shape[1]
+        box_indices = np.arange(num_boxes, dtype=np.int32)
+
+        chosen_box_indices = np.zeros((num_boxes,), dtype=np.int32)
+        num_chosen_box_indices = 0
+        while num_boxes != 0:
+            max_index = 0
+            cur_max = -1
+            for i in range(num_boxes):
+                i = box_indices[i]
+                cur = bounding_boxes[0, i, 4]
+                if cur > cur_max:
+                    max_index = i
+                    cur_max = cur
+            chosen_box_indices[num_chosen_box_indices] = max_index
+            num_chosen_box_indices += 1
+
+            new_box_indices = np.zeros((num_boxes,), dtype=np.int32)
+            new_num_boxes = 0
+            min_x_1, min_y_1, max_x_1, max_y_1 = bounding_boxes[0, max_index,
+                                                 :4]
+            for i in range(num_boxes):
+                i = box_indices[i]
+                keep = False
+                if i != max_index:
+                    min_x_2, min_y_2, max_x_2, max_y_2 = bounding_boxes[0, i,
+                                                         :4]
+                    if min_x_1 > max_x_2 or min_x_2 > max_x_1:
+                        overlap_x = np.float32(0)
+                    elif min_x_1 <= max_x_2:
+                        overlap_x = max_x_2 - max(min_x_1, min_x_2)
+                    else:
+                        overlap_x = max_x_1 - max(min_x_2, min_x_1)
+                    if min_y_1 > max_y_2 or min_y_2 > max_y_1:
+                        overlap_y = np.float32(0)
+                    elif min_y_1 <= max_y_2:
+                        overlap_y = max_y_2 - max(min_y_1, min_y_2)
+                    else:
+                        overlap_y = max_y_1 - max(min_y_2, min_y_1)
+                    overlap = overlap_x * overlap_y
+                    area_1 = (max_x_1 - min_x_1) * (max_y_1 - min_y_1)
+                    area_2 = (max_x_2 - min_x_2) * (max_y_2 - min_y_2)
+                    iou = overlap / (area_1 + area_2 - overlap)
+                    keep = iou <= nms_threshold
+                if keep:
+                    new_box_indices[new_num_boxes] = i
+                    new_num_boxes += 1
+            box_indices = new_box_indices
+            num_boxes = new_num_boxes
+
+        chosen_box_indices = chosen_box_indices[:num_chosen_box_indices]
+        new_bounding_boxes = np.zeros(
+            (1, num_chosen_box_indices, 5), dtype=np.float32
+        )
+        new_classes = np.zeros(
+            (1, num_chosen_box_indices), dtype=np.int32
+        )
+        for i in range(num_chosen_box_indices):
+            j = chosen_box_indices[i]
+            new_bounding_boxes[0, i, :] = bounding_boxes[0, j, :]
+            new_classes[0, i] = classes[0, j]
+        return new_bounding_boxes, new_classes
+
+    def process(self, raw, identifiers, frame_meta):
+        raw_outputs = self._extract_predictions(raw, frame_meta)
+        box_offsets = np.array(
+            raw_outputs[self.box_offsets_out], dtype=np.float32
+        )
+        class_predictions = np.array(
+            raw_outputs[self.class_predictions_out], dtype=np.float32
+        )
+
+        bounding_boxes, classes = self.get_bounding_boxes(
+            np.int32(1), box_offsets, class_predictions,
+            self.prior_boxes,
+            np.array([.1, .1, .2, .2], dtype=np.float32), np.float32(0.01)
+        )
+        bounding_boxes, classes = self.nms(bounding_boxes, classes, 0.45)
+
+        results = []
+        for identifier, cur_bounding_boxes in zip(identifiers, bounding_boxes):
+            x_mins, y_mins, x_maxs, y_maxs, labels, scores = [], [], [], [], \
+                                                             [], []
+            for x_min, x_max, y_min, y_max, confidence in cur_bounding_boxes:
+                x_mins.append(x_min)
+                y_mins.append(y_min)
+                x_maxs.append(x_max)
+                y_maxs.append(y_max)
+                labels.append(1)
+                scores.append(confidence)
+            results.append(
+                DetectionPrediction(
+                    identifier, labels, scores, x_mins, y_mins, x_maxs, y_maxs
+                )
+            )
+        return results
+
