7abfbb2b1c89dcc126b7dfed196597d94e0c4e5d
RobertChenKFC <robert.chen.kfc@gmail.com>
2022-04-21 13:32:52 +0800

    added batch support for TFLite launcher

--- tools/accuracy_checker/accuracy_checker/launcher/tf_lite_launcher.py
+++ tools/accuracy_checker/accuracy_checker/launcher/tf_lite_launcher.py
@@ -13,6 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import os
+import threading
+from multiprocessing.dummy import Pool
+
 import numpy as np
 import tflite_runtime.interpreter
 
@@ -44,10 +48,29 @@ class TFLiteLauncher(Launcher):
     def __init__(self, config_entry, *args, **kwargs):
         super().__init__(config_entry, *args, **kwargs)
         # Change to use TFLiteModel directly
-        self.model = TFLiteModel(
-            str(self.config["model"]),
-            use_edgetpu=(self.config.get("device") == "TPU")
-        )
+        use_edgetpu = self.config.get("device") == "TPU"
+
+        model_args = {
+            "use_edgetpu": use_edgetpu,
+        }
+        if use_edgetpu:
+            self.pool_threads = 1
+        else:
+            model_args["num_threads"] = 1
+            self.pool_threads = os.cpu_count()
+        thread_local = threading.local()
+
+        def initializer():
+            thread_local.model = self.model = TFLiteModel(
+                str(self.config["model"]),
+                **model_args
+            )
+
+        def run(inputs):
+            return thread_local.model(*inputs)
+
+        self.pool = Pool(self.pool_threads, initializer=initializer)
+        self.run = run
 
         self.tf_lite = tflite_runtime.interpreter
         self.default_layout = 'NHWC'
@@ -82,10 +105,20 @@ class TFLiteLauncher(Launcher):
             list_inputs = []
             for detail in self._input_details:
                 input_tensor_name = detail["name"]
-                list_inputs.append(
-                    dataset_input[input_tensor_name].astype(np.float32)
-                )
-            list_results = self.model(*list_inputs)
+                cur_input = dataset_input[input_tensor_name].astype(np.float32)
+                batch_size = cur_input.shape[0]
+                list_inputs.append(cur_input)
+            pool_inputs = []
+            for i in range(batch_size):
+                pool_inputs.append([
+                    cur_input[i:i + 1] for cur_input in list_inputs
+                ])
+            pool_outputs = self.pool.map(self.run, pool_inputs)
+            list_results = []
+            for i, _ in enumerate(self._output_details):
+                list_results.append(np.concatenate([
+                    cur_output[i] for cur_output in pool_outputs
+                ], axis=0))
 
             res = dict()
             for tensor, detail in zip(list_results, self._output_details):
